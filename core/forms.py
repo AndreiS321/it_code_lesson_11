cf5b6c81d34e5b399134fbd678a30b11f3ea3bbb
Andrei <skibin-03@mail.ru>
2023-05-16 12:03:37 +0500

    Added search forms and search logic in class views

--- core/forms.py
+++ core/forms.py
@@ -3,6 +3,32 @@ from django import forms
 from core import models
 
 
+class CustomerSearch(forms.Form):
+    name = forms.CharField(label="Поиск по имени", required=False)
+
+
+class SellerSearch(forms.Form):
+    name = forms.CharField(label="Поиск по имени", required=False)
+
+
+class ItemSearch(forms.Form):
+    name = forms.CharField(label="Поиск по названию", required=False)
+    price = forms.FloatField(label="Поиск по цене", required=False)
+    currency = forms.ChoiceField(label="Поиск по валюте", choices=(((None, "-"),) + models.Item.currency_choices),
+                                 required=False)
+    seller = forms.ModelChoiceField(
+        queryset=models.Seller.objects.all(), label="Поиск по продавцу", required=False
+    )
+class OrderSearch(forms.Form):
+    customer = forms.ModelChoiceField(
+        queryset=models.Customer.objects.all(), label="Покупатель", required=False
+    )
+    items = forms.ModelMultipleChoiceField(
+        queryset=models.Item.objects.all(), label="Товары", required=False
+    )
+    finished = forms.BooleanField(label="Завершён ли заказ", required=False)
+
+
 class CustomerForm(forms.ModelForm):
     name = forms.CharField(label="Имя")
 
@@ -21,7 +47,7 @@ class SellerForm(forms.ModelForm):
 
 class ItemForm(forms.ModelForm):
     name = forms.CharField(label="Название")
-    price = forms.IntegerField(label="Цена", min_value=0)
+    price = forms.FloatField(label="Цена", min_value=0)
     currency = forms.ChoiceField(label="Валюта", choices=models.Item.currency_choices)
     seller = forms.ModelChoiceField(
         queryset=models.Seller.objects.all(), label="Продавец"
--- core/views.py
+++ core/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-from core.forms import CustomerForm, SellerForm, ItemForm, OrderForm
+from core.forms import CustomerForm, SellerForm, ItemForm, OrderForm, CustomerSearch, SellerSearch, ItemSearch, \
+    OrderSearch
 from core.models import Customer, Item, Seller, Order
 
 
@@ -75,8 +76,15 @@ class CustomerList(ListView):
     model = Customer
     template_name = "customer/customers_list.html"
 
+    def get_queryset(self):
+        name = self.request.GET.get("name")
+        if name:
+            return self.model.objects.filter(name__icontains=name).all()
+        return self.model.objects.all()
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["form"] = CustomerSearch(self.request.GET or None)
         context["table_title"] = "Покупатели"
         context["table_headers"] = ("№", "Имя")
         return context
@@ -86,8 +94,15 @@ class SellerList(ListView):
     model = Seller
     template_name = "seller/sellers_list.html"
 
+    def get_queryset(self):
+        name = self.request.GET.get("name")
+        if name:
+            return self.model.objects.filter(name__icontains=name).all()
+        return self.model.objects.all()
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["form"] = SellerSearch(self.request.GET or None)
         context["table_title"] = "Продавцы"
         context["table_headers"] = ("№", "Имя")
         return context
@@ -97,8 +112,26 @@ class ItemList(ListView):
     model = Item
     template_name = "item/items_list.html"
 
+    def get_queryset(self):
+        name = self.request.GET.get("name")
+        price = self.request.GET.get("price")
+        currency = self.request.GET.get("currency")
+        seller = self.request.GET.get("seller")
+        filters = {
+            "name__icontains": name,
+            "price__gte": price,
+            "currency": currency,
+            "seller": seller,
+        }
+        filters = {key: value for key, value in filters.items() if value}
+        queryset = self.model.objects.all()
+        if filters:
+            return queryset.filter(**filters)
+        return queryset
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["form"] = ItemSearch(self.request.GET or None)
         context["table_title"] = "Товары"
         context["table_headers"] = ("№", "Название", "Стоимость", "Валюта", "Продавец")
         return context
@@ -108,8 +141,24 @@ class OrderList(ListView):
     model = Order
     template_name = "order/orders_list.html"
 
+    def get_queryset(self):
+        customer = self.request.GET.get("name")
+        items = self.request.GET.get("items")
+        finished = self.request.GET.get("finished")
+        filters = {
+            "customer": customer,
+            "items": items,
+            "finished": bool(finished),
+        }
+        filters = {key: value for key, value in filters.items() if value}
+        queryset = self.model.objects.all()
+        print(filters)
+        if filters:
+            return queryset.filter(**filters)
+        return queryset
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["form"] = OrderSearch(self.request.GET or None)
         all_items = tuple(
             ", ".join(item.name for item in order.items.all())
             for order in context["object_list"]
