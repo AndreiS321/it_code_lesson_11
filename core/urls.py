c92c068fd5cf41bdb9bf52df7a79aa4d0ed577ae
Andrei <skibin-03@mail.ru>
2023-05-18 18:57:12 +0500

    Minor changes

--- core/urls.py
+++ core/urls.py
@@ -10,12 +10,12 @@ urlpatterns = [
 
     path("customers/list", core.views.CustomerList.as_view(), name="customers_list"),
     path("customers/create", core.views.CustomerCreate.as_view(), name="customers_create"),
-    path("customers/update/<int:pk>", core.views.CustomerUpdate.as_view(), name="customers_update", ),
+    path("customers/update/<int:pk>", core.views.CustomerUpdate.as_view(), name="customers_update"),
     path("customers/del/<int:pk>", core.views.CustomerDelete.as_view(), name="customers_del", ),
 
     path("sellers/list", core.views.SellerList.as_view(), name="sellers_list"),
     path("sellers/create", core.views.SellerCreate.as_view(), name="sellers_create"),
-    path("sellers/update/<int:pk>", core.views.SellerUpdate.as_view(), name="sellers_update", ),
+    path("sellers/update/<int:pk>", core.views.SellerUpdate.as_view(), name="sellers_update"),
     path("sellers/del/<int:pk>", core.views.SellerDelete.as_view(), name="sellers_del"),
 
     path("items/list", core.views.ItemList.as_view(), name="items_list"),
